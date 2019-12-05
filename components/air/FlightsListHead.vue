cf398db9e9433a6ad9fdec98a63bdb20502b602b
yangzhihua <359292310@qq.com>
2019-12-05 15:15:05 +0800

    飞机头显示

--- components/air/FlightsListHead.vue
+++ components/air/FlightsListHead.vue
@@ -1,33 +1,43 @@
 <template>
-  <div class="flight-item">
-    <el-row type="flex" align="middle" class="flight-info">
-      <el-col :span="6"> <span>东航 </span> MU5316 </el-col>
-        <el-col :span='12'>
+    <el-row class="flight-title">
+        <el-col :span="5">
+            航空信息
+        </el-col>
+        <el-col :span="14">
             <el-row type="flex" justify="space-between">
-                <el-col :span="8">
-                    <p>20:30</p>
-                    <span>白云机场</span>
+                <el-col :span="12">
+                    起飞时间
                 </el-col>
-                <el-col :span="8">
-                    <p>2时30分</p>
-                </el-col>
-                <el-col :span="8">
-                    <p>20:50</p>
-                    <span>红椒机场</span>
+                <el-col :span="12">
+                    到达时间
                 </el-col>
             </el-row>
         </el-col>
-        <el-col :span="6"> 
-            ￥<span>810</span>起
-         </el-col>
+        <el-col :span="5">
+            价格
+        </el-col>
     </el-row>
-  </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+}
 </script>
 
-<style lang='less'>
+<style scoped lang="less">
+    .flight-title{
+        padding:0 15px;
+        border:1px #ddd solid;
+        background:#f6f6f6;
+        height:38px;
+        line-height: 38px;
+        color:#666;
+        font-size:12px;
+        margin-bottom: 10px;
 
+        > div{
+            text-align: center
+        }
+    }
 </style>
